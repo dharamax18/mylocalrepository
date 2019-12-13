12f9f7cc487cd329fa3a673bc8a11b1f516779fb
dharm <dharm@DESKTOP-SLM3NCO>
2019-12-13 13:16:13 +0530

    this is my first commit

---
+++ ArrayDemo/src/com/scpn/java/array/ArrayDemo.java
@@ -0,0 +1,23 @@
+package com.scpn.java.array;
+
+public class ArrayDemo {
+
+	
+	public static void main(String[] args) {
+		
+		int [] arr = {8,4,8,2,6,8,8,1,8,3,8};
+		int max = 0;
+		int count = 1;
+		for(int i = 0;i<arr.length;i++) {
+			if(arr[i] > max) {
+				
+				max = arr[i];
+				
+			}else if(arr[i] == max) {
+				count++;
+			}
+		}
+		System.out.println("max number is "+max+" and is present in array "+count+" times");
+	}
+	
+}
